4c8fc67331d4cbe8ecb0c4df7d1d745aeb325886
Parabeac-Nest <admin@parabeac.com>
2022-06-23 04:15:48 +0000

    Auto-generated commit by Parabeac Nest

---
+++ lib/widgets/page_1/helloworld.g.dart
@@ -0,0 +1,18 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+Container(width: 561.000,height: 43.000,child: AutoSizeText(
+'Hello world',
+style: TextStyle(
+fontFamily: 'Inter',
+fontSize: 96,
+fontWeight: FontWeight.w400,
+letterSpacing: 0,
+color: Colors.white,
+),textAlign: TextAlign.left,
+
+))
